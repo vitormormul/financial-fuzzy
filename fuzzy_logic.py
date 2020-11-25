fec424633701fdb4ca04c8aba333cfde8853af62
Vitor <vitormormul@gmail.com>
2020-11-24 21:30:59 -0300

    Inclusao do sistema fuzzy

--- fuzzy_logic.py
+++ fuzzy_logic.py
@@ -1,4 +1,5 @@
 from fuzzy_system.fuzzy_variable_input import FuzzyInputVariable
+from fuzzy_system.fuzzy_system import FuzzySystem
 from fuzzy_system.key import Key
 
 
@@ -7,11 +8,9 @@ pvp = Key(-8.4, 1.2, 1.9, 4.4, 21.6)
 div_yield = Key(0, 0.4, 1.5, 3.9, 14.6)
 roe = Key(-1159.1, 4.8, 10.5, 17.1, 526.8)
 
-#
-# pl_input = FuzzyInputVariable('P/L', pl.minimum, pl.maximum, 1_000_000)
-# pl_input.add_trapezoidal('Negativo', pl.minimum, pl.bottom, pl.zero, pl.above_zero)
-# pl_input.add_triangular('Baixo', pl.below_zero, pl.zero, pl.mean)
-# pl_input.add_triangular('Medio', pl.lower_quartile, pl.mean, pl.upper_quartile)
-# pl_input.add_triangular('Alto', pl.mean, pl.top, pl.maximum)
+system = FuzzySystem()
+system.add_input_variable(pl.input)
+system.add_input_variable(pvp.input)
+system.add_input_variable(div_yield.input)
+system.add_input_variable(roe.input)
 
-print(type(roe.input))
