c4515ee0c36434d5d7c71c31011ae4861a490107
Vitor <vitormormul@gmail.com>
2020-11-24 21:55:27 -0300

    Atualizacao do sistema fuzzy + correcao para yield nao negativo

--- fuzzy_logic.py
+++ fuzzy_logic.py
@@ -1,16 +1,30 @@
 from fuzzy_system.fuzzy_variable_input import FuzzyInputVariable
 from fuzzy_system.fuzzy_system import FuzzySystem
 from fuzzy_system.key import Key
+from fuzzy_system.fuzzy_variable_output import FuzzyOutputVariable
 
 
-pl = Key(-329.4, 4.8, 14.6, 26, 462.3)
-pvp = Key(-8.4, 1.2, 1.9, 4.4, 21.6)
-div_yield = Key(0, 0.4, 1.5, 3.9, 14.6)
-roe = Key(-1159.1, 4.8, 10.5, 17.1, 526.8)
+pl = Key('P/L', -329.4, 4.8, 14.6, 26, 462.3)
+pvp = Key('P/VPA', -8.4, 1.2, 1.9, 4.4, 21.6)
+div_yield = Key('Yield', 0, 0.4, 1.5, 3.9, 14.6, negative=False)
+roe = Key('ROE', -1159.1, 4.8, 10.5, 17.1, 526.8)
+
+investment = FuzzyOutputVariable('Investimento', 0, 100, 1_000_000)
+investment.add_trapezoidal('Fraco', 0, 0.1, 25, 50)
+investment.add_triangular('Avaliar', 25, 50, 75)
+investment.add_trapezoidal('Fraco', 50, 75, 99.9, 100)
+
+price = FuzzyOutputVariable('Preco', 0, 100, 1_000_000)
+price.add_trapezoidal('Favoravel', 0, 0.1, 25, 50)
+price.add_triangular('Avaliar', 25, 50, 75)
+price.add_trapezoidal('Desfavoravel', 50, 75, 99.9, 100)
 
 system = FuzzySystem()
 system.add_input_variable(pl.input)
 system.add_input_variable(pvp.input)
 system.add_input_variable(div_yield.input)
 system.add_input_variable(roe.input)
+system.add_output_variable(investment)
+system.add_output_variable(price)
 
+system.plot_system()
--- fuzzy_system/key.py
+++ fuzzy_system/key.py
@@ -1,15 +1,15 @@
 from .fuzzy_variable_input import FuzzyInputVariable
-
+from .fuzzy_variable_output import FuzzyOutputVariable
 
 class Key:
-    def __init__(self, minimum, lower_quartile, mean, upper_quartile, maximum):
+    def __init__(self, name, minimum, lower_quartile, mean, upper_quartile, maximum, negative=True):
         self.minimum = minimum
         self.lower_quartile = lower_quartile
         self.mean = mean
         self.upper_quartile = upper_quartile
         self.maximum = maximum
         self.zero = 0
-        self.input = self._input()
+        self.input = self._input(name, negative)
 
     @property
     def bottom(self):
@@ -27,9 +27,10 @@ class Key:
     def above_zero(self):
         return self.zero + 0.1
 
-    def _input(self):
-        fuzzy_variable = FuzzyInputVariable('P/L', self.minimum, self.maximum, 1_000_000)
-        fuzzy_variable.add_trapezoidal('Negativo', self.minimum, self.bottom, self.zero, self.above_zero)
+    def _input(self, name, negative):
+        fuzzy_variable = FuzzyInputVariable(name, self.minimum, self.maximum, 1_000_000)
+        if negative:
+            fuzzy_variable.add_trapezoidal('Negativo', self.minimum, self.bottom, self.zero, self.above_zero)
         fuzzy_variable.add_triangular('Baixo', self.below_zero, self.zero, self.mean)
         fuzzy_variable.add_triangular('Medio', self.lower_quartile, self.mean, self.upper_quartile)
         fuzzy_variable.add_triangular('Alto', self.mean, self.top, self.maximum)
